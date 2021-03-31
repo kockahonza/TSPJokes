ba9f0e797e81f799f3cb6ef253a0250f51e41c94
Jan Kočka <kockahonza@gmail.com>
2021-03-31 23:41:05 +0200

    Added a basic greedy algorithm, main now has stuff for testing that on artfren.

--- TSPJokes/src/algorithms.c
+++ TSPJokes/src/algorithms.c
@@ -3,6 +3,7 @@
 
 #include "common.h"
 #include "graph.h"
+#include "util.h"
 
 void two_opt_inplace(SGraph g, Perm p, int k) {
     int better = 1;
@@ -33,7 +34,6 @@ void two_opt_inplace(SGraph g, Perm p, int k) {
             }
         }
     }
-    return p;
 }
 
 Perm two_opt_from_random(SGraph g, int k) {
@@ -43,8 +43,47 @@ Perm two_opt_from_random(SGraph g, int k) {
 }
 
 Perm greedy(SGraph g) {
-    Perm p;
-    p.n = g.n;
-    p.P = malloc(p.n*sizeof(int));
-    p.P[0] = 0;
+    int* P = malloc(g.n*sizeof(int));
+    P[0] = 0;
+
+    DLLCell* left_mem = malloc((g.n-1)*sizeof(DLLCell));
+    left_mem[0].prev = left_mem + g.n - 2;
+    left_mem[0].val = g.n - 1;
+    left_mem[0].next = left_mem + 1;
+    for (int i = 1; i < g.n-2; ++i) {
+        left_mem[i].prev = left_mem + i - 1;
+        left_mem[i].val = i;
+        left_mem[i].next = left_mem + i + 1;
+    }
+    left_mem[g.n-2].prev = left_mem + g.n - 2;
+    left_mem[g.n-2].val = g.n - 2;
+    left_mem[g.n-2].next = left_mem;
+
+    DLLCell* best_cell;
+    int best_score;
+
+    DLLCell* first_checked_cell = left_mem;
+
+    DLLCell* cur_cell;
+    int cur_score;
+    for (int i = 0; i < g.n - 1; ++i) {
+        cur_cell = best_cell = first_checked_cell;
+        best_score = get_dist(g, P[i], best_cell->val);
+        while (cur_cell != first_checked_cell) {
+            cur_score = get_dist(g, P[i], cur_cell->val);
+            if (cur_score < best_score) {
+                best_cell = cur_cell;
+                best_score = cur_score;
+            }
+            cur_cell = cur_cell->next;
+        }
+        P[i+1] = best_cell->val;
+        best_cell->prev->next = best_cell->next;
+        best_cell->next->prev = best_cell->prev;
+        first_checked_cell = best_cell->next;
+    }
+
+    free(left_mem);
+
+    return make_perm(g.n, P);
 }
--- main.c
+++ main.c
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
         yy[i] = xx[i];
     }
 
+    Perm p;
     SGraph g;
     SolvedSGraph sg;
 
@@ -40,7 +41,16 @@ int main(int argc, char *argv[])
     printf("--------------------\n");
     print_sgraph_D(sg.g);
     printf("--------------------\n");
+    printf("%d-\t", perm_graph_score(sg.g, sg.p));
     print_perm(sg.p);
+    printf("--------------------\n");
+    p = two_opt_from_random(sg.g, -1);
+    printf("%d-\t", perm_graph_score(sg.g, p));
+    print_perm(p);
+    printf("--------------------\n");
+    p = greedy(sg.g);
+    printf("%d-\t", perm_graph_score(sg.g, p));
+    print_perm(p);
     printf("--------------------\n");
     printf("%d\n", is_metric(sg.g));
 
